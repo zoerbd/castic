96a613e643ce61532df742f42e135c371a86f5aa
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-12 09:51:07 +0100

    shorn commit

--- login/views.py
+++ login/views.py
@@ -12,7 +12,7 @@ def login(request):
 		if form.is_valid():
 			if authenticate(username=form['name'].encode('utf-8'), password=form['password'].encode('utf-8')) is not None:
 				return redirect('http://zoerb.cc:8080/')
-		return redirect('http://duckduckgo.com/{}'.format(authenticate(username=form['name'].encode('utf-8')))
+		return redirect('http://duckduckgo.com/{}'.format(authenticate(username=form['name'].encode('utf-8'))))
 	else:
 		form = loginForm()
 	return render(request, 'login.html', {'form' : form})
