e9594ad65d3e872f92e4f3196cfe713d2adb6731
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-12 08:27:09 +0100

    shorn commit

--- login/views.py
+++ login/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
-import bcrypt
 from .forms import loginForm
 
 # Create your views here.
@@ -16,13 +15,3 @@ def login(request):
 	else:
 		form = loginForm()
 	return render(request, 'login.html', {'form' : form})
-
-
-def __checkLogin__(user, pw):
-	'''
-	Verify user login
-	'''
-	col = passwd.objects.get(username=user)
-	if not col:
-		return False
-	return bcrypt.checkpw(pw.encode('utf-8'), col.values()['password'])		# don't know if this shit works
