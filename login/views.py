77b3552917811959bc2ca048059b683a44205ba8
root <root@zoerb.cc>
2019-02-12 09:05:01 +0000

    shorn commit

--- login/views.py
+++ login/views.py
@@ -10,14 +10,7 @@ def login(request):
 	if request.method == 'POST':
 		form = loginForm(request.POST)
 		if form.is_valid():
-<<<<<<< HEAD
-			print(form.fields['name'])
-			print(form.fields['password'])
-			return redirect('http://duckduckgo.com')
-			if authenticate(username=form['name'], password=form['password']) is not None:
-=======
 			if authenticate(username=form['name'].value(), password=form['password'].value()) is not None:
->>>>>>> c7067e0896cb6f661ab9a6cdb1e5f6f227879ab9
 				return redirect('http://zoerb.cc:8080/')
 		return redirect('http://duckduckgo.com/{}'.format(form['name'].value()))
 	else:
