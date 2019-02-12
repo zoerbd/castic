3208acd265016380d66a70bfcc167c5915207095
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-12 10:06:13 +0100

    shorn commit

--- login/views.py
+++ login/views.py
@@ -12,7 +12,7 @@ def login(request):
 		if form.is_valid():
 			if authenticate(username=form['name'].value(), password=form['password'].value()) is not None:
 				return redirect('/')
-		return redirect('http://duckduckgo.com/{}'.format(form['name'].value()))
+		return redirect('http://duckduckgo.com/invalid-login')
 	else:
 		form = loginForm()
 	return render(request, 'login.html', {'form' : form})
