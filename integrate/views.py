25b98b96476839340b41dd3c3b4c2c10af3f9390
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-15 16:59:48 +0100

    shorn commit

--- integrate/views.py
+++ integrate/views.py
@@ -9,6 +9,9 @@ def integrate(request):
 	Backend for ansible based automated integration
 	'''
 	if request.method == 'POST':
+		form = integrateInformation(request.POST)
+		if form.is_valid():
+			__renderAnsible__(form['user'].value(), form['password'].value(), form['dest'].value())
 		return redirect('/')
 	return render(request, 'integrate.html', {"form": integrateInformation()})
 
@@ -18,3 +21,11 @@ def __shell__(command):
         This function makes it less pain to get shell answers
         '''
         return subprocess.check_output(command, shell=True).decode('utf-8')
+
+def __renderAnsible__(user, pw, dest):
+		'''
+		This function renders the ansible configuration and roles 
+		and executes it on after that on remote machine.
+		Integration is based on my shell script to integrate restic.
+		'''
+		return
