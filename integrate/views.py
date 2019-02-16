0b6561e028e3ca0329ac49ebe7d60de8dce69c56
root <root@zoerb.cc>
2019-02-16 10:53:28 +0000

    shorn commit

--- integrate/views.py
+++ integrate/views.py
@@ -14,15 +14,14 @@ def integrate(request):
 	'''
 	if request.method == 'POST':
 		form = integrateInformation(request.POST)
-		print(form.is_valid)
 		if form.is_valid():
 			files = __renderAnsible__(form['user'].value(), form['password'].value(), 
-			form['dest'].value(), form['resticPassword'].value(), form['repoPath'].value())
-			return render(request, 'checkOutput.html', {'output':files[0]})
+			form['dest'].value(), form['repoPath'].value())
+			return render(request, 'checkOutput.html', {'output':files})
 		return redirect('/')
 	if config['general']['backupPath'][-1] != '/':
 		config['general']['backupPath'] += '/'
-	form = integrateInformation(initial={'repoPath': 'autofill from dest coming soon!'})
+	form = integrateInformation() #initial={'repoPath': 'autofillComingSoon!'})
 	return render(request, 'integrate.html', {"form":form, "config":config})
 
 
@@ -32,14 +31,14 @@ def __shell__(command):
         '''
         return subprocess.check_output(command, shell=True).decode('utf-8')
 
-def __renderAnsible__(user, pw, dest, resticPW, repoPath):
+def __renderAnsible__(user, pw, dest, repoPath, resticPW = None):
 		'''
 		This function renders the ansible configuration and 
 		roles and executes it on after that on remote machine.
 		Integration is based on my shell script to integrate restic.
 		'''
 		# read recursively all ansible-files, return list with [<path>, <content>] for each file
-		files = [ [os.path.join(root, filename), os.path.join(root, filename).readlines()] 
+		files = [ [os.path.join(root, filename), open(os.path.join(root, filename)).readlines()] 
 		for root, subdirs, filenames in os.walk('./integrate/ansible') 
 		for filename in filenames ]
 		return files
