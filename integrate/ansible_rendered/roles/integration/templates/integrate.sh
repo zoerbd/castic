b390133361962b191c8a0f48021431277e0bae50
root <root@zoerb.cc>
2019-02-16 18:08:19 +0000

    shorn commit

--- integrate/ansible_rendered/roles/integration/templates/integrate.sh
+++ integrate/ansible_rendered/roles/integration/templates/integrate.sh
@@ -1,1 +1,1 @@
-restic -r sftp:"??ownHost??":"??repoPath??" backup "/etc/" --password-file /etc/restic.pw
+restic -r sftp:"??ownHost??":"??repoPath??" backup "remotepath" --password-file /etc/restic.pw
