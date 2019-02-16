756a11abc4c397146823d4dea367704cfeb0c22c
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-16 11:24:02 +0100

    shorn commit

--- integrate/forms.py
+++ integrate/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
 class integrateInformation(forms.Form):
+	# ordinary text input
 	user = forms.CharField(label='Remote-user', max_length=16)
-	password = forms.CharField(widget=forms.PasswordInput())
 	dest = forms.CharField(label='dest', max_length=16)
-	resticPassword = forms.CharField(widget=forms.PasswordInput())
 	repoPath = forms.CharField(label='repoPath', max_length=16)
+
+	# passwords
+	password = forms.CharField(widget=forms.PasswordInput())
+	#resticPassword = forms.CharField(widget=forms.PasswordInput())
+
+	# check boxes
 	ownPassword = forms.BooleanField(required=False)
