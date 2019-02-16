56772051ab3562a778b45d46fe39cb2fd4fd0d6d
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-16 17:21:57 +0100

    shorn commit

--- integrate/tests.py
+++ integrate/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .forms import integrateInformation
 
 # Create your tests here.
+class FormTest(TestCase):
+	def testForms(self):
+		formData = {
+			'user':'zoerbdo',
+			'password':'AdminAdmin',
+			'dest':'zoerb.ccd',
+			'repoPath':'/var/backup/',
+			'backupPath':'/etc/'
+			}
+		form = integrateInformation(data=formData)
+		self.assertTrue(form.is_valid())
