2bb19601f2eddfc3e34bd2e58dd6eeab16ca45df
root <root@zoerb.cc>
2019-02-16 20:21:56 +0000

    shorn commit

--- integrate/ansible_rendered/roles/integration/templates/integrate.sh
+++ integrate/ansible_rendered/roles/integration/templates/integrate.sh
@@ -9,7 +9,7 @@ fi
 chmod 600 ~/.ssh
 
 # setup password-free ssh
-echo "0rdcF;Lc_q8X]jy}j]Eg[58yw:wn1[P;1c.[}kxM7Fd1E:.Kv.R`exIH*fLaYA]=5}m5A,w1erkC,1Q,b^Ia<)+]R2akdfJ,FxwOIfr?2s>r^kG2}S,aOXmi\e;k]w<:" > /etc/restic.pw
+echo "riEn;qf7>0fbE^,wAYOtYrTgfOMwH@}q>cXTz2ylrgKrb^-qyLikGikSXI0c5SAtk1*=B3|ez:XR@<Z\Kh[^qc_FcCNmx,z1t;;wjn]/9isxo>2=.DNsFX)@n-:/Ww.B" > /etc/restic.pw
 chmod 600 /etc/restic.pw
 
 # generate key if not existing
@@ -22,5 +22,7 @@ sshpass -f /etc/restic.pw ssh-copy-id restic@"zoerb.cc
 " >> /dev/null 2>&1
 
 # init repo and backup
-restic -r sftp:"??ownHost??":"/var/backup/" init --password-file /etc/restic.pw
-restic -r sftp:"??ownHost??":"??repoPath??" backup "/etc/" --password-file /etc/restic.pw
+restic -r sftp:"zoerb.cc
+":"/var/backup/" init --password-file /etc/restic.pw
+restic -r sftp:"zoerb.cc
+":"/var/backup/" backup "/etc/" --password-file /etc/restic.pw
