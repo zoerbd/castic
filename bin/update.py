1a79828db2eade02475eed4888728575bb07836a
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-19 20:46:42 +0100

    shorn: Added first implementation of AutoCheck

---
+++ bin/update.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os, sys
+sys.path.insert(0, '/var/www/castic/') # insert path to import project-stuff (make more dynamic later on)
+
+from django.conf import settings
+from django import setup
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webmanagement.settings")
+setup()
+from update.check import checkRepositories
+
+help = "This command is mainly called from cron update automation."
+checkRepositories()
