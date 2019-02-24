0b739488896d960061c38780af715d84be908226
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-02-24 15:15:18 +0100

    shorn: Added basic implementation of inspecting snapshot

--- src/snapshots/models.py
+++ src/snapshots/models.py
@@ -5,6 +5,6 @@ from django.utils.timezone import now
 class snapshots(models.Model):
 	snapshotID = models.CharField(max_length=8)
 	created = models.DateTimeField()
-	backupedPath = models.CharField(max_length=32)
 	host = models.CharField(max_length=32)
+	backupedPath = models.CharField(max_length=32)
 	absolPath = models.ForeignKey('repositories.repositories', on_delete=models.CASCADE)
--- src/snapshots/tests.py
+++ src/snapshots/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+class FormTest(TestCase):
+	def testForms(self):
+		response = self.client.get("/snapshots/.var.backup.serverNo1/")
