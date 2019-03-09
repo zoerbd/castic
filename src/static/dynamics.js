7329443b92b50f9f551cd37285c47ef7ff778dee
root <root@teching.dev>
2019-03-09 08:13:01 +0000

    shorn commit

--- src/static/dynamics.js
+++ src/static/dynamics.js
@@ -7,4 +7,17 @@ $(document).ready(function(){
 	}, function() {
 			$(this).css('box-shadow', '0 0 15px rgba(0,0,0,0.2)');
 	});
+
+	let genBoxes = $('.genBoxSmall');
+	let last_was_floated = false;
+	for (let j=0; j<genBoxes.length; j++){	
+		if ( !last_was_floated ) {
+			genBoxes[j].className += ' right';
+		}
+		if ( j % 2 == 0 ){
+			last_was_floated = true;
+			continue;
+		}
+		last_was_floated = false;
+	}
 })
--- src/update/mailing.py
+++ src/update/mailing.py
@@ -18,33 +18,7 @@ class mailingNotification():
 		'''
 		if not self.config['sendMailNotifications']:
 			return 'Not sending mail, because notifications are disabled in settings.'
-		if not self.__checkMailingConfig__():
-			return 'Error in config-syntax detected.\nExiting.'
 		return self.__renderAndSendMail__()
-
-	def __checkMailingConfig__(self):
-		'''
-		This method gets called by manageMailing().
-		It checks if the config is valid.
-		If that's the case, it returns True.
-		'''
-		# define all used regex-patterns
-		mailPattern = re.compile(r'\w+\@[\w+\.\w+|localhost]')
-		domainPattern = re.compile(r'^([.*\.\w+|localhost])$')
-		sizePattern = re.compile(r'^(\d+[A-Z])$')
-
-		# check if mail addresses are valid
-		matches = []
-		matches.append(mailPattern.finditer(self.config['mailAddress']))
-		matches.append(mailPattern.finditer(self.config['mailFrom']))
-		matches.append(domainPattern.finditer(self.config['smtpServer']))
-		matches.append(sizePattern.finditer(self.config['warnIfDiskSpaceSmallerThan']))
-
-		# ---------> implement logging here or more verbose output
-		for matchgroup in matches:
-			if not [match.group(0) for match in matchgroup]:
-				return False
-		return True
 
 	def __renderAndSendMail__(self):
 		'''
