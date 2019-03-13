272efcbf86d0375c018c99f36d88c65b9555450d
zoerbd <40120132+zoerbd@users.noreply.github.com>
2019-03-13 10:22:43 +0100

    fixed minor integration problems

--- src/integrate/forms.py
+++ src/integrate/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 class integrateInformation(forms.Form):
 	# ordinary text input
-	user = forms.CharField(label='Remote-user', max_length=16)
-	dest = forms.CharField(label='dest', max_length=16)
-	repoPath = forms.CharField(label='repoPath', max_length=16)
-	backupPath = forms.CharField(label='backupPath', max_length=16)
+	user = forms.CharField(label='Remote-user', max_length=32)
+	dest = forms.CharField(label='dest', max_length=64)
+	repoPath = forms.CharField(label='repoPath', max_length=32)
+	backupPath = forms.CharField(label='backupPath', max_length=32)
 
 	# passwords
 	password = forms.CharField(widget=forms.PasswordInput())
